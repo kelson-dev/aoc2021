a3962302e3e0c780d809357e99098bebddf3f356
Kelson Ball <kelson.j.ball@gmail.com>
2021-12-02 23:07:17 -0800

    Organize
    Organize

---
+++ Day3/Part1.cs
@@ -0,0 +1,28 @@
+﻿namespace Day3;
+using Common;
+using System.Collections.Generic;
+
+public readonly record struct Part1 : ISolution<int[], uint>
+{
+    public int InputDay => 3;
+
+    public uint Evaluate(IEnumerable<int[]> inputs)
+    {
+        int[]? counts = null;
+        int lines = 0;
+        foreach (var vec in inputs)
+        {
+            lines++;
+            counts ??= new int[vec.Length];
+            for (int i = 0; i < vec.Length; i++)
+                counts[i] += vec[i];
+        }
+        int half = lines / 2;
+        uint gamma = 0;
+        int len = counts?.Length ?? 0;
+        for (int bit = 0; bit < len; bit++)
+            gamma |= (counts![len - bit - 1] > half ? 1u : 0u) << bit;
+        uint epsilon = ~gamma & (~0u >> (32 - len));
+        return gamma * epsilon;
+    }
+}
---
+++ Day3/Part2.cs
@@ -0,0 +1,42 @@
+﻿namespace Day3;
+using Common;
+using System.Collections.Generic;
+
+public readonly record struct Part2 : ISolution<(int, uint), uint>
+{
+    public int InputDay => 3;
+
+    // Partician numbers into 2 sets based on the state of 'bit'
+    // Return both sets with the largest first
+    private (HashSet<uint>, HashSet<uint>) Split(HashSet<uint> numbers, int bit)
+    {
+        uint mask = 1u << bit;
+        var (set, unset) = (new HashSet<uint>(), new HashSet<uint>());
+        foreach (var num in numbers)
+            ((num & mask) == 0 ? unset : set).Add(num);
+        return set.Count >= unset.Count ? (set, unset) : (unset, set);
+    }
+
+    public uint Evaluate(IEnumerable<(int, uint)> inputs)
+    {
+        var numbers = new HashSet<uint>();
+        var width = -1;
+        foreach (var (number_of_bits, number) in inputs)
+        {
+            if (width == -1)
+                width = number_of_bits;
+            numbers.Add(number);
+        }
+
+        var index = width - 1;
+        var (o2, co2) = Split(numbers, index--);
+        // continue splitting the larger set down to 1 item
+        while (o2.Count > 1 && index >= 0)
+            (o2, _) = Split(o2, index--);
+        index = width - 2;
+        // continue splitting the smaller set down to 1 item
+        while (co2.Count > 1 && index >= 0)
+            (_, co2) = Split(co2, index--);
+        return o2.Single() * co2.Single();
+    }
+}
